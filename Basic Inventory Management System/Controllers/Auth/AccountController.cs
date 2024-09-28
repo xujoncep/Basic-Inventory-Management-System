154e5707fd158486aca348e42d550fc22377075c
sujoncep <sujoncep@gmail.com>
2024-09-29 01:26:49 +0600

    User Authentication: Controller Added For Registation and Login

---
+++ Basic Inventory Management System/Controllers/Auth/AccountController.cs
@@ -0,0 +1,88 @@
+﻿using DataAccessLayer.ViewModels.Auth.Account;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ServicesLayer.IService;
+
+namespace Basic_Inventory_Management_System.Controllers.Auth
+{
+    public class AccountController : Controller
+    {
+        private readonly IAccountService _accountService;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(IAccountService accountService, SignInManager<IdentityUser> signInManager)
+        {
+            _accountService = accountService;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterUser model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _accountService.RegisterUserAsync(model);
+                if (result.Succeeded)
+                {
+
+                    return RedirectToAction("Login", "Account");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginUser model, string? returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var loginSuccess = await _accountService.LoginUserAsync(model);
+                if (loginSuccess)
+                {
+                    if (!string.IsNullOrEmpty(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _accountService.LogoutUserAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
