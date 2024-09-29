1d01404a95b45b75c665b88427dd249fc74f3bdf
sujoncep <sujoncep@gmail.com>
2024-09-29 14:56:21 +0600

    User Management: ViewModels created

---
+++ DataAccessLayer/ViewModels/UserManagement/RoleOfUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.ViewModels.UserManagement
+{
+    public class RoleOfUser
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool IsSelected { get; set; }
+
+    }
+}
---
+++ DataAccessLayer/ViewModels/UserManagement/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.ViewModels.UserManagement
+{
+    public class User
+    {
+
+        public string Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
