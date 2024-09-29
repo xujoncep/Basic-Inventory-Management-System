23b9b466d474e3e92880062694d32afd50f80eb5
sujoncep <sujoncep@gmail.com>
2024-09-30 00:52:37 +0600

    Supplier: Entity Added

---
+++ DataAccessLayer/Migrations/20240929184942_AddSupplierModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSupplierModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Supplier",
+                columns: table => new
+                {
+                    SupplierId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SupplierName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    SupplierCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    ContactInfo = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    IsActive = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Supplier", x => x.SupplierId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Supplier");
+        }
+    }
+}
---
+++ DataAccessLayer/Models/Supplier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Supplier
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SupplierId { get; set; }
+
+        [Required]
+        [DisplayName("Supplier Name")]
+        [StringLength(50)]
+        public string SupplierName { get; set; }
+
+        [Required]
+        [DisplayName("Supplier Code")]
+        [StringLength(10)]
+        public string SupplierCode { get; set; }
+
+        [Required]
+        [DisplayName("Contact Info")]
+        [StringLength(25)]
+        public string ContactInfo { get; set; }
+
+        /// For 1=Active,  0=InActive
+        [Required]
+        public int IsActive { get; set; }
+    }
+}
