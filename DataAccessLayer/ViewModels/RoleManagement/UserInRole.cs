73f4a329059ae9fd9cea8860879befc9acef97ef
sujoncep <sujoncep@gmail.com>
2024-09-29 10:55:14 +0600

    Role Management: ViewModels created

---
+++ DataAccessLayer/ViewModels/RoleManagement/UserInRole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.ViewModels.RoleManagement
+{
+    public class UserInRole
+    {
+        [Required(ErrorMessage = "User id is required.")]
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage = "User name is required.")]
+        public string UserName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
